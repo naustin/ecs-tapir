7cb2b1b03c56026e35e9163e04d8bb062f7f5639
Noel Austin <noel_austin@optum.com>
2024-02-08 13:50:18 -0500

    update exposed port

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "app_image" {
 
 variable "app_port" {
   description = "Port exposed by the docker image to redirect traffic to"
-  default     = 80
+  default     = 8080
 }
 
 variable "app_count" {
